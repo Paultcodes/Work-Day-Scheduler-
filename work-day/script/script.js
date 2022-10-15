846607b7380a212149edb47b2c5a344719f07fab
Paultcodes <paultayloremail1995@gmail.com>
2022-10-14 20:29:00 -0500

    first commit. I setup the time blocks in the HTML

---
+++ work-day/script/script.js
@@ -0,0 +1,10 @@
+var timeDisplayEl = $('#time-display');
+
+
+function displayTime() {
+    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+  }
+
+
+  setInterval(displayTime, 1000);
