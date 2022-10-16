d4c0c0548888bd3df0cc0c835d1fc0b83c011345
Paultcodes <paultayloremail1995@gmail.com>
2022-10-16 01:29:11 -0500

    Backgrounds change color now depending on the time of day

--- work-day/script/script.js
+++ work-day/script/script.js
@@ -1,10 +1,29 @@
-var timeDisplayEl = $('#time-display');
-
+var timeDisplay = $("#time-display");
+var today = moment();
+var allId = document.querySelectorAll("div");
 
 function displayTime() {
-    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-    timeDisplayEl.text(rightNow);
+  var rightNow = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
+  timeDisplay.text(rightNow);
+}
+
+var checkHour = moment().hour();
+console.log(checkHour);
+
+setInterval(displayTime, 1000);
+
+function changeColor() {
+  for (let i = 0; i < allId.length; i++) {
+    const element = allId[i];
+    var idNums = parseInt(element.getAttribute("id"));
+    if (idNums === checkHour) {
+      element.classList.add("present");
+    } else if (idNums > checkHour) {
+      element.classList.add("future");
+    } else {
+      element.classList.add("past");
+    }
   }
+}
 
-
-  setInterval(displayTime, 1000);
+changeColor();
